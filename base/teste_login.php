94bcd84d174a6b341c6b6cb5d6d33a393694555a
Vinycius Marques <vinyciuscezar@outlook.com>
2024-11-08 16:36:09 -0300

    alteração simples

--- base/teste_login.php
+++ base/teste_login.php
@@ -1,19 +1,13 @@
 <?php
-
-if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']));
-{
-    include_once('config.php');
+if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+    include_once 'config.php';
 
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-echo "Email:" . $email;
-echo "Senha:" . $senha;
-
-}
-else
-{
+    echo "Email: " . $email;
+    echo "Senha: " . $senha;
+} else {
     header('Location: tela-de-login.php');
+    exit(); // Importante: adicione exit() após o redirecionamento
 }
-
-?>
